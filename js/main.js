e6ed2732cf036679655ab7f575c794da379d9230
TroshinSerg <serge-troshin@yandex.ru>
2019-09-24 13:22:21 +0300

    Добавляет отрисовку ПопАпа синформацией о предложении

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 var MAP = document.querySelector('.map');
 var MAP_PIN_LIST = document.querySelector('.map__pins');
 var MAP_PIN_TEMPLATE = document.querySelector('#pin').content.querySelector('.map__pin');
-
+var MAP_CARD_TEMPLATE = document.querySelector('#card').content.querySelector('.map__card');
+var MAP_FILTERS_CONTAINER = MAP.querySelector('.map__filters-container');
 var SIMILAR_AD_COUNT = 8;
 var MAP_WIDTH = MAP.offsetWidth;
 var MAP_MIN_Y = 130;
@@ -99,5 +100,74 @@ function drawPins(parentNode, mocks) {
   parentNode.appendChild(fragment);
 }
 
+function getEndingWord(num, endings) {
+  if (+num % 100 === 0 || +num % 100 > 4 && +num % 100 !== 1) {
+    return endings[2];
+  } else if (+num % 100 === 1) {
+    return endings[1];
+  }
+
+  return endings[0];
+}
+
+function createMapPopup(array) {
+  var fragment = document.createDocumentFragment();
+  var clonedMapPopup = MAP_CARD_TEMPLATE.cloneNode(true);
+  var mapPopupType = clonedMapPopup.querySelector('.popup__type');
+  var mapPopupFeatureList = clonedMapPopup.querySelector('.popup__features');
+  var mapPopupFeatures = mapPopupFeatureList.querySelectorAll('.popup__feature');
+  var mapPopupPhotoList = clonedMapPopup.querySelector('.popup__photos');
+  var mapPopupPhoto = mapPopupPhotoList.querySelector('.popup__photo');
+
+  clonedMapPopup.querySelector('.popup__title').textContent = array[0].offer.title;
+  clonedMapPopup.querySelector('.popup__text--address').textContent = array[0].offer.address;
+  clonedMapPopup.querySelector('.popup__text--price').textContent = array[0].offer.price + '₽/ночь';
+
+  switch (array[0].offer.type) {
+    case 'flat':
+      mapPopupType.textContent = 'Квартира';
+      break;
+    case 'bungalo':
+      mapPopupType.textContent = 'Бунгало';
+      break;
+    case 'house':
+      mapPopupType.textContent = 'Дом';
+      break;
+    case 'palace':
+      mapPopupType.textContent = 'Дворец';
+      break;
+    default:
+      mapPopupType.textContent = 'Другой';
+  }
+
+  clonedMapPopup.querySelector('.popup__text--capacity').textContent = array[0].offer.rooms + ' комнат' + getEndingWord(array[0].offer.rooms, ['ы', 'а', '']) + ' для ' + array[0].offer.guests + ' гост' + getEndingWord(array[0].offer.guests, ['ей', 'я', 'ей']) + '.';
+  clonedMapPopup.querySelector('.popup__text--time').textContent = 'Заезд после ' + array[0].offer.checkin + ', выезд до ' + array[0].offer.checkout;
+
+  mapPopupFeatureList.innerHTML = '';
+  array[0].offer.features.forEach(function (element) {
+    for (var i = 0; i < mapPopupFeatures.length; i++) {
+      if (mapPopupFeatures[i].classList.contains('popup__feature--' + element)) {
+        var clonedFeature = mapPopupFeatures[i].cloneNode(true);
+        mapPopupFeatureList.appendChild(clonedFeature);
+        break;
+      }
+    }
+  });
+
+  clonedMapPopup.querySelector('.popup__description').textContent = array[0].offer.description;
+
+  mapPopupPhotoList.innerHTML = '';
+  array[0].offer.photos.forEach(function (element) {
+    var clonedPhoto = mapPopupPhoto.cloneNode(true);
+    clonedPhoto.src = element;
+    mapPopupPhotoList.appendChild(clonedPhoto);
+  });
+
+  clonedMapPopup.querySelector('.popup__avatar').src = array[0].author.avatar;
+  fragment.appendChild(clonedMapPopup);
+  MAP.insertBefore(fragment, MAP_FILTERS_CONTAINER);
+}
+
 MAP.classList.remove('map--faded');
 drawPins(MAP_PIN_LIST, getMocks(SIMILAR_AD_COUNT));
+createMapPopup(getMocks(SIMILAR_AD_COUNT));
